2e2ef09aedc3c64878f75108badf8c568de171cf
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2020-09-07 14:09:27 +0500

    MenuComponent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap'
+import React, {Component} from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
